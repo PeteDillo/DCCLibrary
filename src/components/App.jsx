3c6e6a7c531f52f903ddb5b1c52cceeb6577e079
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-02 15:54:26 -0500

    Moved book viewing logic into its own file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './App.css'
 import TitleBar from "./TitleBar/TitleBar";
+import BookViewer from "./BookViewer/BookViewer";
 
 class App extends Component {
     constructor(props){
@@ -12,7 +13,7 @@ class App extends Component {
             {title:"Harry Potter and The Sorcerer's Stone", author: 'J.K. Rowling'}
         ];
         this.state = {
-            bookNumber: 2
+            bookNumber: 0
         };
     }
     goToNextBook = () =>{
@@ -38,23 +39,8 @@ class App extends Component {
     render(){
         return(
             <div className="container-fluid">
-                <TitleBar/>
-                <div className="row">
-                <div className="col-md-4">
-                    {/**Buitton to move to last book */}
-                    <button onClick={this.goToPreviousBook}>Previous Book</button>
-                    </div>
-                <div className="col-md-4">
-                    {/** display book with cover here */}
-                    <h1>{this.books[this.state.bookNumber].title}</h1>
-                    <h4>{this.books[this.state.bookNumber].author}</h4>
-                    </div>
-                <div className="col-md-4">
-                    {/**Buitton to move to next book */}
-                    <button onClick={this.goToNextBook}>Next Book</button>
-                    </div>
-                </div>
-        
+                <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
             </div>
         )
     }
